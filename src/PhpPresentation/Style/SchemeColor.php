977f33d4367a3f82e464817341bfb1a05e202cf7
Thiago Rizzo <rizzo.17@outlook.com>
2023-12-05 17:44:53 -0300

    Feat: Added XML reading and writing methods to SchemeColor class.

--- src/PhpPresentation/Style/SchemeColor.php
+++ src/PhpPresentation/Style/SchemeColor.php
@@ -20,6 +20,10 @@ declare(strict_types=1);
 
 namespace PhpOffice\PhpPresentation\Style;
 
+use DOMElement;
+use PhpOffice\Common\XMLReader;
+use PhpOffice\Common\XMLWriter;
+
 class SchemeColor extends Color
 {
     /**
@@ -51,5 +55,47 @@ class SchemeColor extends Color
     public function getShade(): ?string
     {
         return $this->shade;
+    }
+
+    public static function load(XMLReader $xmlReader, DOMElement $node): ?Color
+    {
+        $element = $xmlReader->getElement('a:schemeClr', $node);
+        if (!$element) {
+            return parent::load($xmlReader, $node);
+        }
+
+        $color = new self();
+        $color->setValue($element->getAttribute('val'));
+
+        $elementShade = $xmlReader->getElement('a:shade', $element);
+        if ($elementShade instanceof DOMElement && $elementShade->hasAttribute('val')) {
+            $color->setShade($elementShade->getAttribute('val'));
+        }
+
+        $elementAlpha = $xmlReader->getElement('a:alpha', $element);
+        if ($elementAlpha instanceof DOMElement && $elementAlpha->hasAttribute('val')) {
+            $alpha = $elementAlpha->getAttribute('val') / 1000;
+            $color->setAlpha($alpha);
+        }
+
+        return $color;
+    }
+
+    public function write(XMLWriter $writer): void
+    {
+        $writer->startElement('a:schemeClr');
+        $writer->writeAttribute('val', $this->getValue());
+
+        if ($this->getShade() !== null) {
+            $writer->startElement('a:shade');
+            $writer->writeAttribute('val', $this->getShade());
+            $writer->endElement();
+        }
+
+        $writer->startElement('a:alpha');
+        $writer->writeAttribute('val', $this->getAlpha() * 1000);
+        $writer->endElement();
+
+        $writer->endElement();
     }
 }
